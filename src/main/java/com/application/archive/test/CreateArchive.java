3b506e5e805ae34cd9032783e3b14473c9f8fa52
cool-pants <muban.sg@gmail.com>
2022-07-10 23:24:35 +0530

    [RWA 3] Fix IOException and add IllegalArgumentException

--- src/main/java/com/application/archive/test/CreateArchive.java
+++ src/main/java/com/application/archive/test/CreateArchive.java
@@ -2,6 +2,8 @@ package com.application.archive.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import com.application.archive.Archiver;
 import com.application.archive.ArchiverFactory;
@@ -17,6 +19,9 @@ public class CreateArchive {
 
 		Archiver archiver = ArchiverFactory.createArchiver("zip");
 		try {
+			if(!Files.isWritable(Path.of("destination"))){
+				throw new IllegalArgumentException("Destinatin folder must have write permissions.");
+			}
 			File archive = archiver.create(archiveName, destination, source);
 			System.out.println("Done.");
 		} catch (IOException e) {
