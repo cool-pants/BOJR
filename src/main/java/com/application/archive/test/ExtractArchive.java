fcf50aca6d5bb4f977884a0fb6ac4c36cef5e8e8
cool-pants <muban.sg@gmail.com>
2022-07-10 22:56:44 +0530

    revert changes

--- src/main/java/com/application/archive/test/ExtractArchive.java
+++ src/main/java/com/application/archive/test/ExtractArchive.java
@@ -2,12 +2,8 @@ package com.application.archive.test;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
-import com.application.archive.ArchiveEntry;
 import com.application.archive.ArchiveFormat;
-import com.application.archive.ArchiveStream;
 import com.application.archive.Archiver;
 import com.application.archive.ArchiverFactory;
 
@@ -21,22 +17,10 @@ public class ExtractArchive {
 		
 		File archive = new File( sourceFile );
 		File destination = new File( destinationFolder );
-		Archiver archiver = ArchiverFactory.createArchiver(ArchiveFormat.ZIP);
-		try{
-			ArchiveStream stream = archiver.stream(archive);
-			ArchiveEntry entry;
 
-			while((entry = stream.getNextEntry()) != null) {
-				if(entry.isDirectory()){
-					String path = destinationFolder+"/"+entry.getName();
-					if(!Files.isDirectory(Paths.get(path))){
-						new File(path).mkdirs();
-						continue;
-					}
-				}
-				entry.extract(destination);
-			}
-			stream.close();
+		Archiver archiver = ArchiverFactory.createArchiver(ArchiveFormat.ZIP);
+		try {
+			archiver.extract(archive, destination);
 			System.out.println("Done.");
 		} catch (IOException e) {		
 			e.printStackTrace();
