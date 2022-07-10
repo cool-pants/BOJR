229bc2f9c6a7545e3b804760f001d6f28bdff2af
cool-pants <muban.sg@gmail.com>
2022-07-10 22:53:48 +0530

    [RWA 1] Fix file not found

--- src/main/java/com/application/archive/test/ExtractArchive.java
+++ src/main/java/com/application/archive/test/ExtractArchive.java
@@ -2,8 +2,12 @@ package com.application.archive.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import com.application.archive.ArchiveEntry;
 import com.application.archive.ArchiveFormat;
+import com.application.archive.ArchiveStream;
 import com.application.archive.Archiver;
 import com.application.archive.ArchiverFactory;
 
@@ -17,10 +21,22 @@ public class ExtractArchive {
 		
 		File archive = new File( sourceFile );
 		File destination = new File( destinationFolder );
-
 		Archiver archiver = ArchiverFactory.createArchiver(ArchiveFormat.ZIP);
-		try {
-			archiver.extract(archive, destination);
+		try{
+			ArchiveStream stream = archiver.stream(archive);
+			ArchiveEntry entry;
+
+			while((entry = stream.getNextEntry()) != null) {
+				if(entry.isDirectory()){
+					String path = destinationFolder+"/"+entry.getName();
+					if(!Files.isDirectory(Paths.get(path))){
+						new File(path).mkdirs();
+						continue;
+					}
+				}
+				entry.extract(destination);
+			}
+			stream.close();
 			System.out.println("Done.");
 		} catch (IOException e) {		
 			e.printStackTrace();
